647f82685bf1c1f101e6e06e2be63fbab62f6560
Loaiza, Steven <SLoaiza+geico@geico.com>
2025-06-19 19:50:08 -0400

    Add branching dags

---
+++ dags/branching_taskapi_dags.py
@@ -0,0 +1,87 @@
+from datetime import datetime
+from airflow.decorators import dag, task
+import pandas as pd
+
+from airflow.operators.python import PythonOperator
+from airflow.models import Variable
+
+Variable.set(key='transform', value='filter_two_seaters')
+
+from pathlib import Path
+
+root_dir = Path(__file__).parents[1]
+
+@dag(
+    dag_id='branching_taskapi_dag',
+    start_date=datetime.now(),
+    schedule=None,
+    tags=['python', 'airflow', 'taskapi', 'branching'],
+)
+def branching_taskapi_dag():
+
+    def read_csv_file():
+        df = pd.read_csv(root_dir / 'datasets' / 'car_data.csv')
+        return df.to_json()
+
+    @task.branch # this task will return the task id of the task to be executed
+    def determine_branch():
+        final_output= Variable.get('transform', default_var=None)
+
+        if final_output == 'filter_two_seaters':
+            return 'filter_two_seaters_task'
+        elif final_output == 'filter_fwds':
+            return 'filter_fwds_task'
+        
+    def filter_two_seaters_task(ti):
+        json_data = ti.xcom_pull(task_ids='read_csv_file_task')
+        df = pd.read_json(json_data)
+
+        # filter the dataframe
+        two_seater_df = df[df['Seats'] == 2]
+
+        # push the dataframe to the XComs
+        ti.xcom_push(key='transformed_result', value=two_seater_df.to_json())
+        ti.xcom_push(key='transform_filename', value='two_seaters')
+
+    def filter_fwds_task(ti):
+        json_data = ti.xcom_pull(task_ids='read_csv_file_task')
+        df = pd.read_json(json_data)
+
+        # filter the dataframe
+        fwd_df = df[df['DriveTrain'] == 'FWD']
+
+        # push the dataframe to the XComs
+        ti.xcom_push(key='transformed_result', value=fwd_df.to_json())
+        ti.xcom_push(key='transform_filename', value='fwds')
+
+    def write_csv_results(ti):
+        json_data = ti.xcom_pull(task_ids='transform_result')
+        file_name = ti.xcom_pull(task_ids='transform_filename')
+
+        df = pd.read_json(json_data)
+
+        df.to_csv(root_dir / 'datasets' / f'{file_name}.csv', index=False)
+
+    read_csv_file_task = PythonOperator(
+        task_id='read_csv_file_task',
+        python_callable=read_csv_file,
+    )
+
+    filter_two_seaters_task = PythonOperator(
+        task_id='filter_two_seaters_task',
+        python_callable=filter_two_seaters_task,
+    )
+
+    filter_fwds_task = PythonOperator(
+        task_id='filter_fwds_task',
+        python_callable=filter_fwds_task,
+    )
+
+    write_csv_results_task = PythonOperator(
+        task_id='write_csv_results_task',
+        python_callable=write_csv_results,
+    )
+
+    read_csv_file_task >> determine_branch() >> [filter_two_seaters_task, filter_fwds_task] >> write_csv_results_task
+
+branching_taskapi_dag()

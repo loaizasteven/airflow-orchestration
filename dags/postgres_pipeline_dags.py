3ea2e436c1f209c54752c8d55100af324d5c4aa6
Loaiza, Steven <SLoaiza+geico@geico.com>
2025-06-22 11:40:58 -0400

    Add files for postgres dags

---
+++ dags/postgres_pipeline_dags.py
@@ -0,0 +1,32 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2025, 1, 1),
+    'retries': 0
+}
+
+with DAG(
+    dag_id='postgres_pipeline_dags',
+    description='Running a pipeline using the Postgres Operator',
+    default_args=default_args,
+    tags=['postgres', 'pipeline'],
+    template_searchpath='/Users/SLoaiza/Documents/GitHub/airflow/sql_statements'
+) as dag:
+
+    create_table_customers = SQLExecuteQueryOperator(
+        task_id='create_table_customers',
+        conn_id='postgres_connection',
+        sql='create_table_customers.sql'
+    )
+
+    create_table_purchases = SQLExecuteQueryOperator(
+        task_id='create_table_purchases',
+        conn_id='postgres_connection',
+        sql='create_table_customer_purchases.sql'
+    )
+
+    create_table_customers >> create_table_purchases
